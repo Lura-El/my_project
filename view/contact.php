42705d7eefefb5377c736f75f33bfe850c55e4ce
lura-el <luravictor777@gmail.com>
2025-01-21 12:20:09 +0800

    Delete everything

--- view/contact.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $title = "Contact";
-   // include './head.php';
-?>
-<?php require __DIR__ . '/head.php' ?>
--- view/services.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $title = "Service";
-   // include './head.php';
-?>
-<?php require __DIR__ . '/head.php' ?>
